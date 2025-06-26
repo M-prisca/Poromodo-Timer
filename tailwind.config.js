0ea1a07499004b3bfa196eeee7a6d3ddb2707196
Prisca Masereli <priscamasereli@gmail.com>
2025-06-26 19:26:39 +0200

    Initial commit - setup Pomodoro timer project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+// This file is used to configure Tailwind CSS for the project.
+// It specifies the content files to scan for class names, extends the default theme, and includes no additional plugins.
